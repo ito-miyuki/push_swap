81ea07335c0ab378dcd665e728cc12088f844847
Miyuki Ito <mito@c1r3p9.hive.fi>
2024-02-01 17:48:01 +0200

    removed comment in H file

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mito <mito@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 11:23:26 by mito              #+#    #+#             */
-/*   Updated: 2024/01/30 15:14:44 by mito             ###   ########.fr       */
+/*   Updated: 2024/02/01 17:47:24 by mito             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,63 +32,51 @@ typedef struct s_stack_node
 	struct s_stack_node		*prev;
 }	t_stack_node;
 
-//sort_three.c
 void				sort_three(t_stack_node **a);
 
 t_stack_node		*find_last(t_stack_node *stack);
 void				set_cheapest(t_stack_node *stack);
 
-//stack_init.c
 void				init_stack_a(t_stack_node **a, char **argv, int sp);
 t_stack_node		*get_cheapest(t_stack_node *stack);
 void				prep_for_push(t_stack_node **stack, t_stack_node *top_node,
 						char stack_name);
 
-//sort_stacks.c
 void				sort_stacks(t_stack_node **a, t_stack_node **b);
 
-// init_b_to_a
 void				init_nodes_b(t_stack_node *a, t_stack_node *b);
 
-// stack_utils.c
 int					stack_len(t_stack_node *stack);
 bool				stack_sorted(t_stack_node *stack);
 t_stack_node		*find_last(t_stack_node *stack);
 t_stack_node		*find_min(t_stack_node *stack);
 t_stack_node		*find_max(t_stack_node *stack);
 
-// init_a_to_b
 void				current_index(t_stack_node *stack);
 void				set_cheapest(t_stack_node *stack);
 void				init_nodes_a(t_stack_node *a, t_stack_node *b);
 
-// push.c
 void				pa(t_stack_node **a, t_stack_node **b);
 void				pb(t_stack_node **b, t_stack_node **a);
 
-// swap.c
 void				sa(t_stack_node **a);
 void				sb(t_stack_node **b);
 void				ss(t_stack_node **a, t_stack_node **b);
 
-// rotate.c
 void				ra(t_stack_node **a);
 void				rb(t_stack_node **b);
 void				rr(t_stack_node **a, t_stack_node **b);
 
-// rev_rotate.c
 void				rra(t_stack_node **a);
 void				rrb(t_stack_node **b);
 void				rrr(t_stack_node **a, t_stack_node **b);
 
-// handle_errors.c
 int					error_syntax(char *str_n);
 int					error_duplicate(t_stack_node *a, int n);
 void				free_errors(t_stack_node **a, char **array, int sp);
 void				free_sp_array(char **array);
 void				free_stack(t_stack_node **stack);
 
-//min_on_top.c
 void				min_on_top(t_stack_node **a);
 
 #endif
